39c915e7ebd74926e757154cb567443017efc00f
Charan <charanxmn@gmail.com>
2024-11-07 15:58:46 +0530

    Updated the boilerplate to look a little stylish-ish

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>
-    <h1>Charan's Next14 template</h1>
-    <p>This is a Next14 template created by Charan (https://charan.dev/)</p>
+  return <div className="mx-auto text-center h-screen flex flex-col justify-center">
+    <h1 className="text-3xl font-bold">Charan's Next 14 template</h1>
+    <p>This is a Next 14 template created by Charan (<Link href="https://charan.dev/" target="_blank">https://charan.dev/</Link>)</p>
   </div>;
 }

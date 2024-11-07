8f518995295eebab3aa5571a76e5570c9ddae0e5
Charan <charanxmn@gmail.com>
2024-11-07 16:39:00 +0530

    Modified the link to lool slightly better.

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,14 @@ export default function Home() {
       </div>
       <h1 className="text-3xl font-bold">Charan&apos;s Next 14 template</h1>
       <p>
-        This is a Next 14 template created by Charan (
-        <Link href="https://charan.dev/" target="_blank">
+        This is a Next 14 template created by Charan <br />
+        <Link
+          href="https://charan.dev/"
+          target="_blank"
+          className="underline hover:text-blue-500"
+        >
           https://charan.dev/
         </Link>
-        )
       </p>
     </div>
   );
